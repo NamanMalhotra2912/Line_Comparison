eade49db27bd1ffab7ff6c861ddd7f332bc70001
NamanMalhotra2912 <neerajmalhotra0001@gmail.com>
2021-02-08 16:54:05 +0530

    Check Whether two lines are equal or not

---
+++ Equalaity_of_line.java
@@ -0,0 +1,19 @@
+package Line_Comparison;
+
+public class Equalaity_of_line {
+	
+	public static void main(String[] args) {
+		
+		String line1="Hi This is Java";
+		String line2="Hi This is Java";
+		
+		if (line1==line2) {
+			System.out.println("Both lines are equal");
+		}
+		else {
+			System.out.println("Both lines are not equal");
+		}
+		
+	}
+
+}
